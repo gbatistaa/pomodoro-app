b9058144986a407e74861612f268148b580f2144
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-05-26 12:57:28 -0300

    alocação dos títulos para h5

--- src/components/PomoNav.jsx
+++ src/components/PomoNav.jsx
@@ -3,9 +3,15 @@ import styles from "../styles/pomoNav.module.css";
 export default function PomoNav(params) {
   return (
     <section className={styles.pomoNav}>
-      <div className={styles.pomoStages}>pomodoro</div>
-      <div className={styles.pomoStages}>short break</div>
-      <div className={styles.pomoStages}>long break</div>
+      <div className={styles.pomoStages}>
+        <h5>pomodoro</h5>
+      </div>
+      <div className={styles.pomoStages}>
+        <h5>short break</h5>
+      </div>
+      <div className={styles.pomoStages}>
+        <h5>long break</h5>
+      </div>
     </section>
   );
 }
