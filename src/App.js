29376d28155fde0a6bba045ef339feb3f081c44f
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-05-26 10:39:34 -0300

    adicionados os componentes faltantes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import PomoClock from "./components/PomoClock";
+import PomoConfig from "./components/PomoConfig";
 import PomoNav from "./components/PomoNav";
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
     <div className="App">
       <h3 className="app-title">pomodoro</h3>
       <PomoNav />
+      <PomoClock />
+      <PomoConfig />
     </div>
   );
 }
