e96a98334bbd13c0a2a60c64199a603ca7a982ff
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-05-27 09:47:46 -0300

    adição do título do app e importação do componente PomoConfig

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import PomoClock from "./components/PomoClock";
-import PomoConfig from "./components/PomoConfig";
 import PomoNav from "./components/PomoNav";
+import PomoConfig from "./components/config/components/PomoConfig";
 
 function App() {
   return (
     <div className="App">
-      <h3 className="app-title">pomodoro</h3>
+      <h2 className="app-title">pomodoro</h2>
       <PomoNav />
       <PomoClock />
       <PomoConfig />
