b288cca5005f38dcede57fb713fa04ef7e3a87be
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-05-28 17:40:51 -0300

    adição da funcionalidade do botão de close

--- src/components/config/components/Header.jsx
+++ src/components/config/components/Header.jsx
@@ -1,11 +1,23 @@
+import { useContext } from "react";
 import close from "../../../assets/./images/icon-close.svg";
 import styles from "../css/header.module.css";
+import { DisplayContext } from "./PomoConfig";
 
 export default function Header() {
+  const { displayConfig, setDisplayConfig } = useContext(DisplayContext);
+
+  const handleDisplayConfig = () => {
+    setDisplayConfig(!displayConfig);
+  };
+
   return (
     <header className={styles.settingsHeader}>
       <h1 className={styles.settingsTitle}>Settings</h1>
-      <button type="button" className={styles.closeButton}>
+      <button
+        type="button"
+        className={styles.closeButton}
+        onClick={() => handleDisplayConfig()}
+      >
         <img src={close} alt="close" />
       </button>
     </header>
