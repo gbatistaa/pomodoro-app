a00a3a5eb084d1a389a47f1b6c0e54ad682e0b0c
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-05-28 21:41:22 -0300

    criada a estrutura de manipulação dos states de duração dos stages de pomodoro

--- src/components/config/components/TimeSettings.jsx
+++ src/components/config/components/TimeSettings.jsx
@@ -1,29 +1,92 @@
 import styles from "../css/timeSettings.module.css";
 import increment from "../../../assets/./images/icon-arrow-up.svg";
 import decrement from "../../../assets/./images/icon-arrow-down.svg";
+import { useReducer } from "react";
 
-const pomoStageNames = ["pomodoro", "short-break", "long-break"];
+const pomoStageNames = ["pomodoro", "shortBreak", "longBreak"];
 
 function TimeInputs({ stageNames }) {
+  const initialTimeState = {
+    pomodoro: 25,
+    shortBreak: 5,
+    longBreak: 10,
+  };
+
+  const reducer = (state, { type, increase }) => {
+    switch (type) {
+      case "set-pomodoro":
+        console.log(state);
+        return {
+          ...state,
+          pomodoro: increase ? state.pomodoro + 1 : state.pomodoro - 1,
+        };
+
+      case "set-shortBreak":
+        return {
+          ...state,
+          shortBreak: increase ? state.shortBreak + 1 : state.shortBreak - 1,
+        };
+
+      case "set-longBreak":
+        return {
+          ...state,
+          longBreak: increase ? state.longBreak + 1 : state.longBreak - 1,
+        };
+
+      default:
+        break;
+    }
+  };
+
+  const [timeStates, dispatch] = useReducer(reducer, initialTimeState);
+
+  const handleTimeIncrease = (event, timeName, currentValue) => {
+    event.preventDefault();
+    if (currentValue < 60) {
+      dispatch({ type: `set-${timeName}`, increase: true });
+    }
+  };
+
+  const handleTimeDecrease = (event, timeName, currentValue) => {
+    event.preventDefault();
+    if (currentValue > 0) {
+      dispatch({ type: `set-${timeName}`, increase: false });
+    }
+  };
+
   return stageNames.map((name, key) => {
     return (
-      <div className={styles.labelContainer}>
+      <div className={styles.labelContainer} key={key}>
         <h5 className={styles.stageName}>{name}</h5>
-        <label htmlFor={name} className={styles.inputLabel} key={key}>
-          <div className={styles.buttonContainer}>
-            <button type="button" className={styles.increment}>
-              <img src={increment} alt="arrow-up" />
-            </button>
-            <button type="button" className={styles.decrement}>
-              <img src={decrement} alt="arrow-down" />
-            </button>
-          </div>
+        <label htmlFor={name} className={styles.inputLabel}>
           <input
             type="number"
             name={name}
             className={styles.timeInput}
             id={name}
+            value={timeStates[`${name}`]}
+            readOnly
           />
+          <div className={styles.buttonContainer}>
+            <button
+              type="button"
+              className={styles.increment}
+              onClick={(e) =>
+                handleTimeIncrease(e, name, timeStates[`${name}`])
+              }
+            >
+              <img src={increment} alt="arrow-up" />
+            </button>
+            <button
+              type="button"
+              className={styles.decrement}
+              onClick={(e) =>
+                handleTimeDecrease(e, name, timeStates[`${name}`])
+              }
+            >
+              <img src={decrement} alt="arrow-down" />
+            </button>
+          </div>
         </label>
       </div>
     );
