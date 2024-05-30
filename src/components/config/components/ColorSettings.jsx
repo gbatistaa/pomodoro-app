82de23fc5f121c8a9d719b994fdcb1e655e6727c
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-05-30 16:45:14 -0300

    substituição para um input específico de cor

--- src/components/config/components/ColorSettings.jsx
+++ src/components/config/components/ColorSettings.jsx
@@ -1,5 +1,5 @@
 import styles from "../css/colorSettings.module.css";
-import RadioInput from "./RadioInput";
+import ColorInput from "./ColorInput.jsx";
 
 const colors = ["#f87070", "#70f3f8", "#d881f8"];
 
@@ -8,12 +8,7 @@ export default function ColorSettings() {
     <section className={styles.colorSettings}>
       <h4 className={styles.stageName}>COLOR</h4>
       <div className={styles.inputsContainer}>
-        <RadioInput
-          array={colors}
-          name={`color`}
-          style={styles.colorLabel}
-          text={``}
-        />
+        <ColorInput array={colors} name={`color`} />
       </div>
     </section>
   );
