b59e07ad385cfe9904dd4f941ef273c88014237f
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-05-30 16:40:16 -0300

    sincronização da cor do  arco de da nav de pomodoro com o state de cor e o state de tempo

--- src/components/PomoNav.jsx
+++ src/components/PomoNav.jsx
@@ -1,15 +1,39 @@
 import styles from "../styles/pomoNav.module.css";
+import { ColorContext, TimeContext } from "../App.js";
+import { useContext } from "react";
 
-export default function PomoNav(params) {
+export default function PomoNav() {
+  const { globalColor } = useContext(ColorContext);
+  const { timeState } = useContext(TimeContext);
+
   return (
     <section className={styles.pomoNav}>
-      <div className={styles.pomoStages}>
+      <div
+        className={`${styles.pomoStages} ${
+          timeState === "pomodoro" ? styles.selected : ""
+        }`}
+        style={timeState === "pomodoro" ? { backgroundColor: globalColor } : {}}
+      >
         <h5>pomodoro</h5>
       </div>
-      <div className={styles.pomoStages}>
+      <div
+        className={`${styles.pomoStages} ${
+          timeState === "shortBreak" ? styles.selected : ""
+        }`}
+        style={
+          timeState === "shortBreak" ? { backgroundColor: globalColor } : {}
+        }
+      >
         <h5>short break</h5>
       </div>
-      <div className={styles.pomoStages}>
+      <div
+        className={`${styles.pomoStages} ${
+          timeState === "longBreak" ? styles.selected : ""
+        }`}
+        style={
+          timeState === "longBreak" ? { backgroundColor: globalColor } : {}
+        }
+      >
         <h5>long break</h5>
       </div>
     </section>
