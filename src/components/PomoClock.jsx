04809885024084b8b9d51176d7a60f16e84e9315
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-05-30 16:39:16 -0300

    sincronização da cor do  arco de duração com o state de cor

--- src/components/PomoClock.jsx
+++ src/components/PomoClock.jsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
 import styles from "../styles/pomoClock.module.css";
+import { ColorContext } from "../App.js";
 
 export default function PomoClock() {
+  const { globalColor } = useContext(ColorContext);
+  const time = new Date();
   return (
-    <div className={styles.pomoClock}>
-      <h1 className={styles.pomoTime}>17:30</h1>
+    <div
+      className={styles.pomoClock}
+      style={{ border: `10px solid ${globalColor}` }}
+    >
+      <h1 className={styles.pomoTime}>
+        {time.getHours()}:{time.getMinutes()}
+      </h1>
       <button type="button" className={styles.pauseButton}>
         PAUSE
       </button>
