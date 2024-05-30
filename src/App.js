2b29d448680052c12be167910f7d6490426f645a
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-05-30 16:37:47 -0300

    criação de state para cor, criação do contexto de tempo, e envolucro de contextos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import { createContext, useState } from "react";
 import "./App.css";
 import PomoClock from "./components/PomoClock";
 import PomoNav from "./components/PomoNav";
 import PomoConfig from "./components/config/components/PomoConfig";
 
+export const ColorContext = createContext();
+export const TimeContext = createContext();
+
 function App() {
+  const [globalColor, setGlobalColor] = useState("#f87070");
+  const [timeState, setTimeState] = useState("longBreak");
+
   return (
     <div className="App">
       <h1 className="app-title">pomodoro</h1>
-      <PomoNav />
-      <PomoClock />
-      <PomoConfig />
+      <TimeContext.Provider value={{ timeState, setTimeState }}>
+        <ColorContext.Provider value={{ globalColor, setGlobalColor }}>
+          <PomoNav />
+          <PomoClock />
+          <PomoConfig />
+        </ColorContext.Provider>
+      </TimeContext.Provider>
     </div>
   );
 }
