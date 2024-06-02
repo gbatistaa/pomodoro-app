98a85f49a4e1019e6a9d36b66ff051fe1ec5724e
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-02 14:53:17 -0300

    sincorinzação dos elementos do componente Pomo Nav com o state global de cor

--- src/components/PomoNav.jsx
+++ src/components/PomoNav.jsx
@@ -1,37 +1,37 @@
 import styles from "../styles/pomoNav.module.css";
-import { ColorContext, TimeContext } from "../App.js";
+import { ColorContext, PomoStageContext } from "../App.js";
 import { useContext } from "react";
 
 export default function PomoNav() {
   const { globalColor } = useContext(ColorContext);
-  const { timeState } = useContext(TimeContext);
+  const { pomoStage } = useContext(PomoStageContext);
 
   return (
     <section className={styles.pomoNav}>
       <div
         className={`${styles.pomoStages} ${
-          timeState === "pomodoro" ? styles.selected : ""
+          pomoStage === "pomodoro" ? styles.selected : ""
         }`}
-        style={timeState === "pomodoro" ? { backgroundColor: globalColor } : {}}
+        style={pomoStage === "pomodoro" ? { backgroundColor: globalColor } : {}}
       >
         <h5>pomodoro</h5>
       </div>
       <div
         className={`${styles.pomoStages} ${
-          timeState === "shortBreak" ? styles.selected : ""
+          pomoStage === "shortBreak" ? styles.selected : ""
         }`}
         style={
-          timeState === "shortBreak" ? { backgroundColor: globalColor } : {}
+          pomoStage === "shortBreak" ? { backgroundColor: globalColor } : {}
         }
       >
         <h5>short break</h5>
       </div>
       <div
         className={`${styles.pomoStages} ${
-          timeState === "longBreak" ? styles.selected : ""
+          pomoStage === "longBreak" ? styles.selected : ""
         }`}
         style={
-          timeState === "longBreak" ? { backgroundColor: globalColor } : {}
+          pomoStage === "longBreak" ? { backgroundColor: globalColor } : {}
         }
       >
         <h5>long break</h5>
