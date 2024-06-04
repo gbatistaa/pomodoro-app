6a24c98a69f514da4310f401a443a4decb13ccef
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-04 18:25:40 -0300

    criação de contexto de fontes e mudança dos valores dos Providers para as referências

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useReducer } from "react";
+import { createContext, useRef } from "react";
 import "./App.css";
 import PomoClock from "./components/PomoClock";
 import PomoNav from "./components/PomoNav";
@@ -6,58 +6,35 @@ import PomoConfig from "./components/config/components/PomoConfig";
 
 export const ColorContext = createContext();
 export const PomoStageContext = createContext();
+export const FontContext = createContext();
 export const TimeSettingsContext = createContext();
 
 function App() {
-  const [globalColor, setGlobalColor] = useState("#f87070");
-  const [pomoStage, setPomoStage] = useState("pomodoro");
-
-  const initialTimeState = {
+  const initialTimeValues = {
     pomodoro: 25,
     shortBreak: 5,
     longBreak: 10,
   };
 
-  const reducer = (state, { type, increase }) => {
-    switch (type) {
-      case "set-pomodoro":
-        console.log(state);
-        return {
-          ...state,
-          pomodoro: increase ? state.pomodoro + 1 : state.pomodoro - 1,
-        };
-
-      case "set-shortBreak":
-        return {
-          ...state,
-          shortBreak: increase ? state.shortBreak + 1 : state.shortBreak - 1,
-        };
-
-      case "set-longBreak":
-        return {
-          ...state,
-          longBreak: increase ? state.longBreak + 1 : state.longBreak - 1,
-        };
-
-      default:
-        break;
-    }
-  };
-
-  const [timeStates, dispatch] = useReducer(reducer, initialTimeState);
+  const globalColorRef = useRef("#f87070");
+  const pomoStageRef = useRef("pomodoro");
+  const fontRef = useRef("kumbh");
+  const { current: timeSettingsRef } = useRef(initialTimeValues);
 
   return (
-    <div className="App">
+    <div className={`App ${fontRef.current}`}>
       <h1 className="app-title">pomodoro</h1>
-      <TimeSettingsContext.Provider value={{ timeStates, dispatch }}>
-        <PomoStageContext.Provider value={{ pomoStage, setPomoStage }}>
-          <ColorContext.Provider value={{ globalColor, setGlobalColor }}>
-            <PomoNav />
-            <PomoClock />
-            <PomoConfig />
-          </ColorContext.Provider>
-        </PomoStageContext.Provider>
-      </TimeSettingsContext.Provider>
+      <FontContext.Provider value={fontRef}>
+        <TimeSettingsContext.Provider value={{ timeSettingsRef }}>
+          <PomoStageContext.Provider value={{ pomoStageRef }}>
+            <ColorContext.Provider value={globalColorRef}>
+              <PomoNav />
+              <PomoClock />
+              <PomoConfig />
+            </ColorContext.Provider>
+          </PomoStageContext.Provider>
+        </TimeSettingsContext.Provider>
+      </FontContext.Provider>
     </div>
   );
 }
