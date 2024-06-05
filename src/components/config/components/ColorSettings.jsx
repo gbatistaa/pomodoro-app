9d5a3253428562316bebda6060b6a18f9e2547d0
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-05 12:10:09 -0300

    criação do state de reset

--- src/components/config/components/ColorSettings.jsx
+++ src/components/config/components/ColorSettings.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import styles from "../css/colorSettings.module.css";
 import ColorInput from "./ColorInput.jsx";
 
 const colors = ["#f87070", "#70f3f8", "#d881f8"];
 
 export default function ColorSettings() {
+  const [, forceUpdate] = useState(0);
+
   return (
     <section className={styles.colorSettings}>
       <h4 className={styles.stageName}>COLOR</h4>
       <div className={styles.inputsContainer}>
-        <ColorInput array={colors} name={`color`} />
+        <ColorInput array={colors} name={`color`} forceUpdate={forceUpdate} />
       </div>
     </section>
   );
