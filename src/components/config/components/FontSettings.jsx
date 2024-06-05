e22672f7edfecdc7e8c18497b6888d08e5ccb7de
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-06-05 13:28:54 -0300

    remoçao de hooks inutilizados

--- src/components/config/components/FontSettings.jsx
+++ src/components/config/components/FontSettings.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import styles from "../css/fontSettings.module.css";
 
 const fontsArray = ["kumbh", "roboto", "space"];
